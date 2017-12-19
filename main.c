8167d7a60f6194834bec470f7bb6be457c05e2ae
Martin Degeldt <15076@student.ecam.be>
2017-12-19 21:07:31 +0100

    Create main.c and algo

---
+++ main.c
@@ -0,0 +1,20 @@
+ 
+/*
+chopper les infos des textes
+
+créer la mains et les board (qui sont que des listes de cartes)
+
+voir si il y a un taunt en face -> ses pv
+
+si dmg + attaque total (charge de la main et windfury, faire *2) < vie + pv rant -> false
+
+-> teser si dégat parfait sur les taunts
+	-> si pas, faire au plus proche
+
+-> degats restant > vie -> true
+
+###
+Si on a le temps, bien print le board
+
+How to Lethal
+###
