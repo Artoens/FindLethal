b992b1a01aab681e3d7a5396bb8bf4ca7270eab0
Martin Degeldt <15076@student.ecam.be>
2017-12-20 18:03:31 +0100

    Start script

--- main.c
+++ main.c
@@ -18,3 +18,39 @@ Si on a le temps, bien print le board
 
 How to Lethal
 ###
+
+*/
+
+#include "hand.h"
+#include "board.h"
+#include "cardlist.h"
+#include "utils.h"
+
+STATE* newState(char *myBoard, char *enemiBoard, char *hand)
+{
+	STATE* state = malloc (sizeof (STATE));
+	state->hand = loadHand(*hand);
+	state->enemi = loadBoard(*enemiBoard);
+	state->mine = loadBoard(*myBoard);
+
+	return state;
+}
+
+int main(int argc, char const *argv[])
+{
+	STATE* state = newState()
+	double sizeEnemi = sizeof(state->enemi->cards)/sizeof(CARD);
+	int tauntHP = 0;
+	CARD** taunts = newCardlist();
+
+	for (int i = 0; i < sizeEnemi; ++i)
+	{
+		CARD this = state->enemi->cards[i];
+		if (this->mechanic == "t")
+			tauntHP += this->life;
+	}
+
+
+	for(i = 0;)
+	return 0;
+}
